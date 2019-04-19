c572fba05ff0bc784bbd63de02152b07f2093278
Troy Hicks <NOLAWolfe@users.noreply.github.com>
2019-04-19 11:20:43 -0500

    Angular running

--- TheSpeed/src/app/app-routing.module.ts
+++ TheSpeed/src/app/app-routing.module.ts
@@ -4,14 +4,11 @@ import { LoginComponent } from './core/login/login.component';
 import { RegisterComponent } from './core/register/register.component';
 import { LandingComponent } from './core/landing/landing.component';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { UserComponent } from './user/user/user.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  {
-    path: 'user',
-    loadChildren: '../user/user.module#UserModule',
-  },
   {
     path: 'landing', component: LandingComponent,
     children: [
